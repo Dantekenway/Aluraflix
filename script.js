6f641181d9facb73a9944c5edace045513f64cfa
João franca <joao.hanter.f@gmail.com.br>
2022-04-14 13:18:18 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var listaAnimes = [
+  //demonslayer:
+  "https://media.fstatic.com/YARUI0UjDJRnALHKnU0-4E9H8ZU=/268x386/smart/media/movies/covers/2019/08/Capa.jpg",
+  //jujutsukaisen//
+  "https://media.fstatic.com/xTAkfPNQGAQvYseIwjbRrSpJXIQ=/290x478/smart/media/movies/covers/2020/09/Nova-imagem-promocional-da-serie-anime-Jujutsu-Kaisen-1.jpg",
+  //resleved
+  "https://4.bp.blogspot.com/-nihxe5rcy-U/Xmq7w11VJMI/AAAAAAABWy4/WbcQ8TnLjzMbt_84Vt57TpuQ7lOwqXBXgCLcBGAsYHQ/s1600/anime-altered-carbon-nova-capa-dublado-em-portugus.jpg",
+  //tenkinoko
+  "https://www.animesxis.com.br/wp-content/uploads/2019/05/Tenki-no-Ko-visual-2-anime-xis.jpg"
+];
+for (var i = 0; i < listaAnimes.length; i++) {
+  document.write("<img src=" + listaAnimes[i] + ">");
+}
